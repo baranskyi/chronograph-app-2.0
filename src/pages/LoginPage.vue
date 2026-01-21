603ef92c61ca2ae5ff4ffe9b5ab347016b260137
Slava Baranskyi <woofer.ua@gmail.com>
2026-01-21 17:11:41 +0100

    Fix: remove unused imports in LoginPage

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue'
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '../stores/authStore'
 
