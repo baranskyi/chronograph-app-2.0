cbf04b232f8dc46cb379a88931c1b3fc7ea7037d
Slava Baranskyi <woofer.ua@gmail.com>
2026-01-21 13:35:59 +0100

    Remove version badge from bottom right corner
    - Removed global version display from App.vue
    - Version still visible in footer on dashboard page
    - Version 1.6.8

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,7 @@
 import { onMounted } from 'vue'
 import { useAuthStore } from './stores/authStore'
 
-declare const __APP_VERSION__: string
-
 const authStore = useAuthStore()
-const appVersion = __APP_VERSION__
 
 onMounted(() => {
   authStore.initialize()
@@ -15,9 +12,5 @@ onMounted(() => {
 <template>
   <div class="h-full">
     <router-view />
-    <!-- Version badge for deploy verification -->
-    <div class="fixed bottom-2 right-2 text-xs text-gray-600 opacity-50">
-      v{{ appVersion }}
-    </div>
   </div>
 </template>
